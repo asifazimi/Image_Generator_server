7922c60cfd58f4d5a6f912970d515d9af06c42bf
Asif Azimi <asifazimi620@hotmail.com>
2023-03-28 20:36:26 +0430

    Connect with mongodb

---
+++ mongodb/models/post.js
@@ -0,0 +1,12 @@
+import mongoose, { Mongoose } from "mongoose";
+
+// how does the data look like in the database
+const post = new mongoose.Schema({
+  name: { type: String, required: true },
+  prompt: { type: String, required: true },
+  photo: { type: String, required: true },
+});
+
+const PostSchema = mongoose.model("Post", post);
+
+export default PostSchema;
