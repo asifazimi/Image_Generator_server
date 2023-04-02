2d64cdbed5f97c520dd40c60b803f4992795c981
Asif Azimi <asifazimi620@hotmail.com>
2023-04-02 19:15:11 +0430

    Add postRoutes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,4 +8,40 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUNDINARY_CLOUD_NAME,
+  cloud_key: process.env.CLOUNDINARY_CLOUD_KEY,
+  cloud_secret: process.env.CLOUNDINARY_CLOUD_SECRET,
+});
+
+// GET ALL POST
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+// CREATE A POST
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    //   Create a post in the database
+    const newPost = await Post.create({
+      name,
+      photo: photoUrl,
+      prompt,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
 export default router;
